98042613c71972d91b7b1d8a96c618d7103c56a6
ashot.shushanyan <ashot.s@yoocollab.com>
2022-08-19 16:50:57 +0400

    commit

--- app/Http/Controllers/FeedbackController.php
+++ app/Http/Controllers/FeedbackController.php
@@ -3,21 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Feedback;
+use App\Http\Requests\Feedback\StoreRequest;
 use App\Http\Services\EmailService;
-use Illuminate\Http\Request;
 
 class FeedbackController extends Controller
 {
-    public function store(Request $request)
+    public function store(StoreRequest $request)
     {
-        $data = $request->validate([
-            'name'  => 'required',
-            'phone' => 'required',
-            'email' => 'email|nullable',
-        ]); // it's also can be StoreRequest
-
         try {
-            $feedback = Feedback::create($data);
+            $feedback = Feedback::create($request->all());
             EmailService::sendFeedback($feedback);
 
             return response([
--- app/Http/Services/EmailService.php
+++ app/Http/Services/EmailService.php
@@ -9,7 +9,6 @@ class EmailService
     const TO_EMAIL = 'customer@feedback.com';
 
     public static function sendFeedback($data) {
-//        dd($data->toArray());
         Mail::send('emails.feedback', $data->toArray(), function ($message) {
             $message
                 ->to(self::TO_EMAIL)
